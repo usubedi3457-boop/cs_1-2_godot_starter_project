0b6014102e6dbc1bbf75e949b3ddbd3adb608c67
usubedi3457-boop <usubedi3457@student.pps.net>
2025-09-29 09:59:16 -0700

    projectile

---
+++ scripts/enemy_projectile.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+var direction : Vector2
+var  speed = 350.0
+
+
+func set_direction(target:Vector2):
+		direction = target
--- scripts/turret.gd
+++ scripts/turret.gd
@@ -1,4 +1,6 @@
 extends Node2D
+const MyProjectile  = preload("res://scenes/enemy_projectile.tscn")
+
 
 # TODO: Add variables for turret state
 # Track if player is in range, projectile scene, etc.
@@ -12,13 +14,17 @@ func _ready():
 	# Look at "Working with Timers" documentation
 	pass
 
-func _on_detection_area_body_entered(body):
+func _on_detection_area_body_entered(_body):
 	# TODO: Check if the body is the player
 	# Update player detection state
 	# Look at "Boolean Variables and State Tracking" documentation
+	if _body.name == "player":
+		print ("turret fired")
+		
 	pass
 
-func _on_detection_area_body_exited(body):
+
+func _on_detection_area_body_exited(_body):
 	# TODO: Check if the body is the player  
 	# Update player detection state
 	# Look at "Boolean Variables and State Tracking" documentation
@@ -30,10 +36,15 @@ func _on_shooting_timer_timeout():
 	# Look at documentation for shooting logic
 	pass
 
-func shoot_at_player():
+func shoot_at_player(body):
 	# TODO: Get player's current position
 	# Calculate direction from turret to player
 	# Create and launch projectile
 	# Look at "Getting Object Positions", "Direction Calculation", 
 	# and "Reusing Projectile Systems" documentation
+	var new_instance = MyProjectile.instantiate()
+	new_instance.set.direction(body.position)
+	get_parent().add_child(new_instance)
+	new_instance.global_position = position
 	pass
+	
