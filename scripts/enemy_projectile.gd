5fb190e57f71cdae2ccc70a0f3f1b97baf6e11a8
usubedi3457-boop <usubedi3457@student.pps.net>
2025-10-03 09:57:36 -0700

    turret does damage

--- scripts/enemy_projectile.gd
+++ scripts/enemy_projectile.gd
@@ -1,16 +1,26 @@
 extends Area2D
-
+var start_time = 2
+var timer = start_time
 var direction
 var speed= 300
-
+var player
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
-	pass
+	if body.name == "Player":
+		body.change_health(-1)
+		queue_free()
+		
+		pass
 
 func _physics_process(_delta):
 	position += direction * speed * _delta
 
 func set_direction(target):
 	direction = position.direction_to(target)
+	
+func _process(delta: float) -> void:
+	timer -= delta 
+	if timer <0:
+		queue_free()
--- scripts/turret.gd
+++ scripts/turret.gd
@@ -3,13 +3,22 @@ var projectile_original = preload("res://scenes/enemy_projectile.tscn")
 @export var offset : Vector2 = Vector2(0, -25)
 
 var facing
-
+var start_time = 2
+var timer =  start_time
+var in_range = false
+var player
 
 func _ready():
 	
 	pass
 
 func _process(delta: float) -> void:
+	
+	if in_range:
+		timer -= delta
+		if timer < 0:
+			shoot(player)
+			timer = start_time
 	pass
 
 
@@ -17,14 +26,17 @@ func _process(delta: float) -> void:
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.name=="Player":
-		shoot(body)
-	if body.name == "Player":
-		body.change_health(-2)
+	if body.name == "Player": 
+		player = body
+		in_range = true
 	pass # Replace with function body.
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		@warning_ignore("standalone_expression")
+		player == null
+		in_range = false
 	pass # Replace with function body.
 	
 func shoot(body):
