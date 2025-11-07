b52d641fca7ebd904225f1f04c431ac0b86be4a8
usubedi3457-boop <usubedi3457@student.pps.net>
2025-11-07 09:57:42 -0800

    minotaur chasing/attacking player

--- scripts/enemy_projectile.gd
+++ scripts/enemy_projectile.gd
@@ -6,17 +6,17 @@ var speed= 300
 var player
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
-
+	
 func _on_body_entered(body):
 	if body.name == "Player":
 		body.change_health(-1)
 		queue_free()
 		
 		pass
-
+	
 func _physics_process(_delta):
-	position += direction * speed * _delta
-															 
+	position += direction * speed * _delta 
+	
 func set_direction(target):
 	direction = position.direction_to(target)
 	
--- scripts/melee_minotaur.gd
+++ scripts/melee_minotaur.gd
@@ -1,27 +1,53 @@
 extends CharacterBody2D
 var in_range = false
 var attacking = false
-var speed = 200
+var speed = 250
 var health  = 3
 var chasing = false
+var direction = Vector2(0,0)
+var start_time = 1
+var timer =  start_time
+var _direction = Vector2.ZERO
+
+	
 @onready var player: CharacterBody2D = %Player
-var projectile_original = preload("res://scenes/enemy_projectile.tscn")
+var projectile_original = preload("res://scenes/enemy_projectile2.tscn")
+
+
+
+func _ready():
+	pass
+
+func set_direction(new_direction: Vector2):
+	_direction = new_direction
+
+	
+
 
 func _process(delta: float) -> void:
+	
 	if in_range:
+		timer -= delta
+	if timer < 0:
+		shoot()
+		timer = start_time
+	
 		pass
 	elif chasing and !attacking:
 		velocity = Vector2.ZERO
-		Direction = position.direction_to(player.position)
+		direction = position.direction_to(player.position)
+		position += direction * delta * speed 
 		
 	elif !chasing and !attacking:
 		pass
 		
-	if chasing == true:
-		position =+ direction * delta * speed attacking
+
+	
 	
 	if !in_range and chasing:
 		print("chasing, not in range")
+	
+	
 	
 
 func _on_melee_body_entered(body: Node2D) -> void:
@@ -34,20 +60,22 @@ func _on_melee_body_entered(body: Node2D) -> void:
 func _on_melee_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
 		attacking = false 
-		
+		chasing = true 
 
 
 func _on_chase_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		chasing = true
 		attacking = false
+		in_range = false
 
 func _on_chase_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
 		chasing = false
-
+		in_range = true
 
 func _on_ranged_body_entered(body: Node2D) -> void:
+	
 	if body.name == "Player":
 		in_range = true
 		attacking = false
@@ -56,3 +84,16 @@ func _on_ranged_body_entered(body: Node2D) -> void:
 func _on_ranged_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
 		in_range = false
+
+func shoot():
+	# TODO: Create a new projectile instance
+	var projectile_clone = projectile_original.instantiate()
+	
+	# TODO: Set projectile position to player position
+	projectile_clone.global_position = position 
+	
+	# TODO: Set projectile direction using facing variable
+	projectile_clone.set_direction(player.position)
+	
+	# TODO: Add projectile to the game world
+	get_tree().get_root().add_child(projectile_clone)
